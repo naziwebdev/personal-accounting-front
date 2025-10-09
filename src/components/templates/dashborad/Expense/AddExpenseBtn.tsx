f418ea66e17d70733cf599909963570f1eab0fb2
nazi <nazipaeez777@gmail.com>
2025-10-09 22:33:33 +0330

    Create add expense form style

--- src/components/templates/dashborad/Expense/AddExpenseBtn.tsx
+++ src/components/templates/dashborad/Expense/AddExpenseBtn.tsx
@@ -1,67 +1,112 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { IconAdd } from "@/components/icons/IconAdd";
+import Modal from "@/components/modules/dashboard/Modal";
+import { IconPaper } from "@/components/icons/IconPaper";
+import { IconCoin } from "@/components/icons/IconCoin";
+import DatePicker from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import "react-multi-date-picker/styles/colors/purple.css";
+import { IconCalender } from "@/components/icons/IconCalender";
+import { IconDescription } from "@/components/icons/IconDescription";
 
 export default function AddExpenseBtn() {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const modalToggleHandle = () => setOpenModal(false);
   return (
     <>
       <button
-        // onClick={() => setOpenCardModal(true)}
+        onClick={() => setOpenModal(true)}
         className="flex justify-center items-center  p-3 lg:p-4 gap-2 mb-10 text-base xs:text-lg lg:text-[18px] bg-[var(--color-secondary)] text-white w-auto h-auto rounded-2xl lg:rounded-3xl shadow-xl cursor-pointer"
       >
         افزودن هزینه جدید
         <IconAdd size="w-6 h-6 lg:w-7 lg:h-7" color="#ffffff" />
       </button>
-      {/* {openCardModal ? (
-        <Modal onClose={cardModalHandle}>
+      {openModal ? (
+        <Modal onClose={modalToggleHandle}>
           <>
             <h2 className="w-1/2 md:w-1/5 mx-auto text-center pb-2 mb-12 text-lg xs:text-2xl font-bold  rounded-xl text-nowrap">
-              افزودن کارت
+              افزودن هزینه
             </h2>
             <form
-              onSubmit={handleSubmit(addCardHandle)}
+              // onSubmit={handleSubmit(addCardHandle)}
               className="px-0 md:px-32 flex items-center justify-center flex-wrap gap-y-5 text-xs xs:text-base"
             >
               <div className="w-full">
-                <input
-                  {...register("bankName")}
-                  type="text"
-                  className="w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
-                  placeholder="نام بانک را وارد کنید"
-                />
-                <span className="text-right pt-1.5  text-sm  text-red-600">
+                <div className="flex items-center gap-2">
+                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
+                    <IconPaper size="w-7 h-7 xs:w-8 xs:h-8" color="#ffffff" />
+                  </div>
+                  <input
+                    type="text"
+                    className="w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
+                    placeholder="عنوان هزینه را وارد کنید"
+                  />
+                </div>
+
+                {/* <span className="text-right pt-1.5  text-sm  text-red-600">
                   {errors.bankName && errors.bankName.message}
-                </span>
+                </span> */}
               </div>
               <div className="w-full">
-                <input
-                  {...register("cardNumber", {
-                    onChange: (e) => {
-                      e.target.value = toEnglishDigits(e.target.value);
-                    },
-                  })}
-                  type="text"
-                  className="w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
-                  placeholder="شماره کارت ۱۶ رقمی را وارد کنید"
-                />
-                <span className="text-right pt-1.5 text-sm  text-red-600">
+                <div className="flex items-center gap-2">
+                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
+                    <IconCoin size="w-7 h-7 xs:w-8 xs:h-8" color="#ffffff" />
+                  </div>
+                  <input
+                    // {...register("cardNumber", {
+                    //   onChange: (e) => {
+                    //     e.target.value = toEnglishDigits(e.target.value);
+                    //   },
+                    // })}
+                    type="text"
+                    className="w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
+                    placeholder="مبلغ هزینه را وارد کنید"
+                  />
+                </div>
+                {/* <span className="text-right pt-1.5 text-sm  text-red-600">
                   {errors.cardNumber && errors.cardNumber.message}
-                </span>
+                </span> */}
               </div>
               <div className="w-full">
-                <input
-                  {...register("balance", {
-                    onChange: (e) => {
-                      e.target.value = toEnglishDigits(e.target.value);
-                    },
-                  })}
-                  type="text"
-                  className="w-full bg-[var(--color-theme)] p-3 placeholder:text-zinc-600 rounded-xl text-zinc-600 outline-0"
-                  placeholder="موجودی کارت را وارد کنید ( اختیاری )"
-                />
-                <span className="text-right pt-1.5  text-sm  text-red-600">
+                <div className="flex items-center gap-2">
+                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
+                    <IconCalender
+                      size="w-6 h-6 xs:w-7 xs:h-7"
+                      color="#ffffff"
+                    />
+                  </div>
+                  <DatePicker
+                    inputClass="custom-input"
+                    className="purple w-full"
+                    placeholder="برای انتخاب تاریخ ضربه بزنید"
+                    // value={value || ""}
+                    // onChange={(date) => {
+                    //   onChange(date);
+                    // }}
+                    locale={persian_fa}
+                    calendar={persian}
+                  />
+                </div>
+                {/* <span className="text-right pt-1.5  text-sm  text-red-600">
                   {errors.balance && errors.balance.message}
-                </span>
+                </span> */}
+              </div>
+              <div className="w-full">
+                <div className="flex items-center gap-2">
+                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-theme)]">
+                    <IconDescription
+                      size="w-6 h-6 xs:w-7 xs:h-7"
+                      color="#52525B"
+                    />
+                  </div>
+                  <textarea
+                    className="w-full bg-[var(--color-theme)] p-3 resize-none placeholder:text-zinc-600 rounded-xl text-zinc-600 outline-0"
+                    placeholder="توضیحات را وارد کنید (اختیاری)"
+                  ></textarea>
+                </div>
               </div>
               <button
                 type="submit"
@@ -72,7 +117,7 @@ export default function AddExpenseBtn() {
             </form>
           </>
         </Modal>
-      ) : null} */}
+      ) : null}
     </>
   );
 }
