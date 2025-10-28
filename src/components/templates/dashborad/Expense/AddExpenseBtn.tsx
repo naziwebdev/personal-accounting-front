4c60c355946567ca9db677e9ff352509d7bb71c8
nazi <nazipaeez777@gmail.com>
2025-10-28 21:45:40 +0330

    Do logic og add expense

--- src/components/templates/dashborad/Expense/AddExpenseBtn.tsx
+++ src/components/templates/dashborad/Expense/AddExpenseBtn.tsx
@@ -4,15 +4,125 @@ import { IconAdd } from "@/components/icons/IconAdd";
 import Modal from "@/components/modules/dashboard/Modal";
 import { IconPaper } from "@/components/icons/IconPaper";
 import { IconCoin } from "@/components/icons/IconCoin";
-import DatePicker from "react-multi-date-picker";
+import DatePicker, { DateObject } from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
 import "react-multi-date-picker/styles/colors/purple.css";
 import { IconCalender } from "@/components/icons/IconCalender";
 import { IconDescription } from "@/components/icons/IconDescription";
+import { useCategoriesByType } from "@/hooks/useCategories";
+import { useCards } from "@/hooks/useCards";
+import { useAuth } from "@/context/AuthContext";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addExpense } from "@/validations/expense";
+import { restoreAccessToken } from "@/utils/restoreAccessToken";
+import { toast } from "sonner";
+import { toEnglishDigits, toPersianDigits } from "@/utils/normalizeDigits";
+import { IconCategory } from "@/components/icons/IconCategory";
+import { IconDownArrow } from "@/components/icons/IconDownAroow";
+import { IconBankCard } from "@/components/icons/IconBankCard";
+
+type addExpenseFormData = {
+  title: string;
+  price: number;
+  date: string;
+  categoryID: number;
+  bankCardID?: number | null;
+  description?: string | null;
+};
 
 export default function AddExpenseBtn() {
   const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const {
+    data: categories,
+    isError,
+    isLoading,
+  } = useCategoriesByType("expense");
+
+  const { data: cards } = useCards();
+
+  const { accessToken, setAccessToken } = useAuth();
+  const queryClient = useQueryClient();
+
+  const {
+    register,
+    reset,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      categoryID: -1,
+      bankCardID: -1,
+      date: "",
+      description: "",
+    },
+    resolver: yupResolver(addExpense),
+  });
+
+  const mutation = useMutation({
+    mutationFn: async (data: addExpenseFormData) => {
+      // Convert Persian date string (e.g. "1404/08/03") to a valid Gregorian Date object
+      // Required because backend expects ISO-formatted Gregorian dates, not Jalali strings
+      const persianDate = new DateObject({
+        date: data.date,
+        format: "YYYY/MM/DD",
+        calendar: persian,
+      });
+      const gregorianDate = persianDate.convert("gregorian" as any).toDate();
+
+      const finalData = {
+        ...data,
+        price: data.price ?? 0,
+        bankCard_id: data.bankCardID === -1 ? null : data.bankCardID,
+        category_id: data.categoryID,
+        date: gregorianDate.toISOString(),
+      };
+      const makeRequest = async (token: string) => {
+        return await fetch("http://localhost:4002/api/v1/expenses", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(finalData),
+        });
+      };
+
+      let res = await makeRequest(accessToken!);
+
+      let result = await res.json();
+
+      if (result.statusCode === 401) {
+        const newToken = await restoreAccessToken();
+        if (!newToken) throw new Error("Unauthorized");
+        setAccessToken(newToken);
+        res = await makeRequest(newToken);
+        result = await res.json();
+      }
+      if (result.statusCode !== 201) throw new Error("Failed to add income");
+
+      return result.data;
+    },
+    onSuccess: () => {
+      toast.success("هزینه با موفقیت افزوده شد");
+      queryClient.invalidateQueries({ queryKey: ["incomes"] });
+      reset();
+      modalToggleHandle();
+    },
+    onError: () => {
+      toast.error("خطا در افزودن هزینه");
+    },
+  });
+
+  const addExpenseHandle = async (data: addExpenseFormData) => {
+    mutation.mutate(data);
+  };
 
   const modalToggleHandle = () => setOpenModal(false);
   return (
@@ -27,50 +137,156 @@ export default function AddExpenseBtn() {
       {openModal ? (
         <Modal onClose={modalToggleHandle}>
           <>
-            <h2 className="w-1/2 md:w-1/5 mx-auto text-center pb-2 mb-12 text-lg xs:text-2xl font-bold  rounded-xl text-nowrap">
+            <h2 className="w-1/2 md:w-1/5 mx-auto text-center pb-2 mb-6 lg:mb-12 text-lg xs:text-2xl font-bold  rounded-xl text-nowrap">
               افزودن هزینه
             </h2>
             <form
-              // onSubmit={handleSubmit(addCardHandle)}
-              className="px-0 md:px-32 flex items-center justify-center flex-wrap gap-y-5 text-xs xs:text-base"
+              onSubmit={handleSubmit(addExpenseHandle)}
+              className="px-4 md:px-20 lg:px-0 flex items-center justify-between lg:justify-evenly flex-wrap gap-y-3  lg:gap-y-8 gap-x-4 lg:gap-x-20 text-xs xs:text-base"
             >
-              <div className="w-full">
+              <div className="w-full lg:max-w-5/12">
                 <div className="flex items-center gap-2">
                   <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
                     <IconPaper size="w-7 h-7 xs:w-8 xs:h-8" color="#ffffff" />
                   </div>
                   <input
+                    {...register("title")}
                     type="text"
                     className="w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
                     placeholder="عنوان هزینه را وارد کنید"
                   />
                 </div>
 
-                {/* <span className="text-right pt-1.5  text-sm  text-red-600">
-                  {errors.bankName && errors.bankName.message}
-                </span> */}
+                <span className="text-right pt-1.5  text-sm  text-red-600">
+                  {errors.title && errors.title.message}
+                </span>
               </div>
-              <div className="w-full">
+              <div className="w-full lg:max-w-5/12">
                 <div className="flex items-center gap-2">
                   <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
                     <IconCoin size="w-7 h-7 xs:w-8 xs:h-8" color="#ffffff" />
                   </div>
                   <input
-                    // {...register("cardNumber", {
-                    //   onChange: (e) => {
-                    //     e.target.value = toEnglishDigits(e.target.value);
-                    //   },
-                    // })}
+                    {...register("price", {
+                      onChange: (e) => {
+                        e.target.value = toEnglishDigits(e.target.value);
+                      },
+                    })}
                     type="text"
                     className="w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
                     placeholder="مبلغ هزینه را وارد کنید"
                   />
                 </div>
-                {/* <span className="text-right pt-1.5 text-sm  text-red-600">
-                  {errors.cardNumber && errors.cardNumber.message}
-                </span> */}
+                <span className="text-right pt-1.5 text-sm  text-red-600">
+                  {errors.price && errors.price.message}
+                </span>
               </div>
-              <div className="w-full">
+              <div className="w-full lg:max-w-5/12">
+                <div className="flex items-center gap-2">
+                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
+                    <IconCategory
+                      size="w-7 h-7 xs:w-8 xs:h-8"
+                      colorBg="#ffffff"
+                      colorIcon="#8c66e5"
+                    />
+                  </div>
+                  <Controller
+                    name="categoryID"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <div className="relative w-full">
+                        <select
+                          {...field}
+                          id="category"
+                          className="appearance-none w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
+                        >
+                          <option
+                            value={"-1"}
+                            className="bg-primary-p text-white"
+                          >
+                            دسته بندی را انتخاب کنید
+                          </option>
+                          {categories?.length !== 0 &&
+                            categories?.map((category) => (
+                              <option
+                                key={category?.id}
+                                value={category?.id}
+                                className="bg-[var(--color-primary)] text-white"
+                              >
+                                {category.title}
+                              </option>
+                            ))}
+                        </select>
+                        <span className="absolute left-3 top-1/2 z-10 transform -translate-y-1/2 pointer-events-none">
+                          <IconDownArrow
+                            size="w-3 h-3 xs:w-4 xs:h-4"
+                            color="#ffffff"
+                          />
+                        </span>
+                      </div>
+                    )}
+                  />
+                </div>
+                <span className="text-right pt-1.5 text-sm  text-red-600">
+                  {errors.categoryID && errors.categoryID.message}
+                </span>
+              </div>
+              <div className="w-full lg:max-w-5/12">
+                <div className="flex items-center gap-2">
+                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-theme)]">
+                    <IconBankCard
+                      size="w-7 h-7 xs:w-8 xs:h-8"
+                      color="#52525B"
+                    />
+                  </div>
+                  <Controller
+                    name="bankCardID"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <div className="relative w-full">
+                        <select
+                          {...field}
+                          value={field.value ?? "-1"}
+                          id="bankCardID"
+                          className="appearance-none w-full p-3 bg-[var(--color-theme)] placeholder:text-zinc-600 rounded-xl text-zinc-600 outline-0"
+                        >
+                          <option
+                            value={"-1"}
+                            className="bg-primary-p text-white"
+                          >
+                            برداشت شده از کارت (اختیاری)
+                          </option>
+                          {cards?.length !== 0 &&
+                            cards?.map((card) => (
+                              <option
+                                key={card?.id}
+                                value={card?.id}
+                                className="bg-[var(--color-primary)] text-white"
+                              >
+                                {card?.bankName} -{" "}
+                                {toPersianDigits(card?.cardNumber)
+                                  .match(/.{1,4}/g)
+                                  ?.join("-")}
+                              </option>
+                            ))}
+                        </select>
+                        <span className="absolute left-3 top-1/2 z-10 transform -translate-y-1/2 pointer-events-none">
+                          <IconDownArrow
+                            size="w-3 h-3 xs:w-4 xs:h-4"
+                            color="#52525B"
+                          />
+                        </span>
+                      </div>
+                    )}
+                  />
+                </div>
+                <span className="text-right pt-1.5 text-sm  text-red-600">
+                  {errors.bankCardID && errors.bankCardID.message}
+                </span>
+              </div>
+              <div className="w-full lg:max-w-5/12">
                 <div className="flex items-center gap-2">
                   <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
                     <IconCalender
@@ -78,23 +294,43 @@ export default function AddExpenseBtn() {
                       color="#ffffff"
                     />
                   </div>
-                  <DatePicker
-                    inputClass="custom-input"
-                    className="purple w-full"
-                    placeholder="برای انتخاب تاریخ ضربه بزنید"
-                    // value={value || ""}
-                    // onChange={(date) => {
-                    //   onChange(date);
-                    // }}
-                    locale={persian_fa}
-                    calendar={persian}
+                  <Controller
+                    control={control}
+                    name="date"
+                    rules={{ required: true }} //optional
+                    render={({ field: { onChange, value } }) => (
+                      <>
+                        <DatePicker
+                          inputClass="custom-input"
+                          className="purple w-full"
+                          format="YYYY/MM/DD"
+                          value={
+                            value
+                              ? new DateObject({
+                                  date: value,
+                                  format: "YYYY/MM/DD",
+                                  calendar: persian,
+                                })
+                              : ""
+                          }
+                          placeholder="برای انتخاب تاریخ ضربه بزنید"
+                          onChange={(dateObject) => {
+                            const formatted =
+                              dateObject?.format?.("YYYY/MM/DD");
+                            onChange(formatted ?? "");
+                          }}
+                          locale={persian_fa}
+                          calendar={persian}
+                        />
+                      </>
+                    )}
                   />
                 </div>
-                {/* <span className="text-right pt-1.5  text-sm  text-red-600">
-                  {errors.balance && errors.balance.message}
-                </span> */}
+                <span className="text-right pt-1.5  text-sm  text-red-600">
+                  {errors.date && errors.date.message}
+                </span>
               </div>
-              <div className="w-full">
+              <div className="w-full lg:max-w-5/12">
                 <div className="flex items-center gap-2">
                   <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-theme)]">
                     <IconDescription
@@ -103,17 +339,23 @@ export default function AddExpenseBtn() {
                     />
                   </div>
                   <textarea
+                    {...register("description")}
                     className="w-full bg-[var(--color-theme)] p-3 resize-none placeholder:text-zinc-600 rounded-xl text-zinc-600 outline-0"
                     placeholder="توضیحات را وارد کنید (اختیاری)"
                   ></textarea>
                 </div>
+                <span className="text-right pt-1.5  text-sm  text-red-600">
+                  {errors.description && errors.description.message}
+                </span>
               </div>
-              <button
-                type="submit"
-                className="mt-7 w-1/2 md:w-1/4 h-10 xs:h-12 flex justify-center items-center text-white rounded-xl bg-[var(--color-primary)] text-base  xs:text-lg cursor-pointer"
-              >
-                تایید
-              </button>
+              <div className="w-full flex justify-center">
+                <button
+                  type="submit"
+                  className="mt-3 lg:mt-7 w-1/3 lg:w-1/5 h-10 xs:h-12 flex justify-center items-center text-white rounded-xl bg-[var(--color-primary)] text-base  xs:text-lg cursor-pointer"
+                >
+                  تایید
+                </button>
+              </div>
             </form>
           </>
         </Modal>
---
+++ src/validations/expense.ts
@@ -0,0 +1,57 @@
+import * as yup from "yup";
+
+export const addExpense = yup.object().shape({
+  title: yup
+    .string()
+    .required("عنوان را وارد کنید")
+    .min(2, "عنوان حداقل ۲ کارکتر باید باشد"),
+  price: yup
+    .number()
+    .transform((value, originalValue) =>
+      originalValue === "" || isNaN(value) ? undefined : value
+    )
+    .required("مبلغ را وارد کنید"),
+
+  categoryID: yup
+    .number()
+    .integer()
+    .required("دسته بندی را مشخص کنید")
+    .test("not-default", "دسته بندی را مشخص کنید", (value) => value !== -1),
+  bankCardID: yup.number().integer().notRequired(),
+
+  date: yup
+    .string()
+    .transform((value) =>
+      value?.replace(/[۰-۹]/g, (d: string) => String("۰۱۲۳۴۵۶۷۸۹".indexOf(d)))
+    )
+    .required("تاریخ درآمد را وارد کنید")
+    .matches(/^\d{4}\/\d{2}\/\d{2}$/, "فرمت تاریخ باید مثل 1404/08/03 باشد")
+
+    .required("تاریخ درآمد را وارد کنید"),
+  description: yup.string().notRequired(),
+});
+
+export const editExpense = yup.object().shape({
+  title: yup.string().notRequired().min(2, "عنوان حداقل ۲ کارکتر باید باشد"),
+  price: yup
+    .number()
+    .transform((value, originalValue) =>
+      originalValue === "" || isNaN(value) ? undefined : value
+    )
+    .notRequired(),
+
+  categoryID: yup
+    .number()
+    .integer()
+    .test("not-default", "دسته بندی را مشخص کنید", (value) => value !== -1)
+    .notRequired(),
+  bankCardID: yup.number().integer().notRequired(),
+
+  date: yup
+    .string()
+    .transform((value) =>
+      value?.replace(/[۰-۹]/g, (d: string) => String("۰۱۲۳۴۵۶۷۸۹".indexOf(d)))
+    )
+    .notRequired(),
+  description: yup.string().notRequired(),
+});
