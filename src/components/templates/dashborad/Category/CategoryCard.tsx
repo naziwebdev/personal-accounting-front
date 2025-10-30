489f5b942dd356729628c311444d5e8d6fd82dfe
nazi <nazipaeez777@gmail.com>
2025-10-31 00:27:11 +0330

    Responsive category page

--- src/components/templates/dashborad/Category/CategoryCard.tsx
+++ src/components/templates/dashborad/Category/CategoryCard.tsx
@@ -10,21 +10,21 @@ type CategoryCardProps = {
 export default function CategoryCard({ color, bgIcon }: CategoryCardProps) {
   return (
     <div
-      className={`relative mt-4 ms-14 flex justify-between items-center w-10/12 p-4 ${color} rounded-xl shadow-lg shadow-zinc-400/50`}
+      className={`relative mt-4 ms-14 flex gap-x-2 justify-between items-center w-2/3 xl:w-10/12 p-4 ${color} rounded-xl shadow-lg shadow-zinc-400/50`}
     >
       <div className="shape absolute w-8 h-[110%] -right-6 -top-1/2 -z-20 border-t-0 border-l-0 border-[3.5px] border-black border-dashed"></div>
-      <div className="flex items-center gap-x-5">
+      <div className="flex items-center gap-x-3 xl:gap-x-5">
         <div
-          className={`flex justify-center items-center w-[4.15rem] h-[4.15rem] rounded-full  shadow-lg shadow-zinc-500/40 ${bgIcon} border-4 border-white border-dotted `}
+          className={`flex justify-center items-center w-14 h-14 md:w-[4.15rem] md:h-[4.15rem] rounded-full  shadow-lg shadow-zinc-500/40 ${bgIcon} border-4 border-white border-dotted `}
         >
-          <IconDollar size="w-10 h-10" color="#fff" />
+          <IconDollar size="w-6 h-6 md:w-10 md:h-10" color="#fff" />
         </div>
-        <p className="text-xl text-shadow-[1px_1px_2px_gray] text-white">
+        <p className="text-base md:text-xl text-shadow-[1px_1px_2px_gray] text-white">
           هدیه
         </p>
       </div>
       <button className="cursor-pointer">
-        <IconActionDot size="w-7 h-7" color="#fff" />
+        <IconActionDot size="w-5 h-5 md:w-7 md:h-7" color="#fff" />
       </button>
     </div>
   );
--- src/components/templates/dashborad/Category/CategoryList.tsx
+++ src/components/templates/dashborad/Category/CategoryList.tsx
@@ -7,15 +7,15 @@ import { IconExpense } from "@/components/icons/category/IconExpense";
 import CategoryCard from "./CategoryCard";
 
 export default function CategoryList() {
-  const iconList = Object.entries(ICONS);
+  // const iconList = Object.entries(ICONS);
   return (
-    <div className="flex justify-between px-32">
-      <section className="w-1/3">
+    <div className="flex flex-wrap sm:flex-nowrap gap-y-10 justify-center sm:justify-between  xl:px-32">
+      <section className="w-full xl:w-1/3">
         <div className="flex items-center gap-4">
-          <div className="flex justify-center items-center w-20 h-20 rounded-full bg-[var(--color-secondary)] shadow-lg shadow-zinc-500/40 ">
-            <IconIncome size="w-14 h-14" color="#fff" />
+          <div className="flex justify-center items-center w-16 h-16 md:w-20 md:h-20 rounded-full bg-[var(--color-secondary)] shadow-lg shadow-zinc-500/40 ">
+            <IconIncome size="w-10 h-10 md:w-14 md:h-14" color="#fff" />
           </div>
-          <h3 className="text-[1.4rem] font-titr text-black text-shadow-[2px_2px_4px_white] text-shadow-xl">
+          <h3 className="text-xl md:text-[1.4rem] font-titr text-black text-shadow-[2px_2px_4px_white] text-shadow-xl">
             دسته بندی واریز ها
           </h3>
         </div>
@@ -46,12 +46,12 @@ export default function CategoryList() {
           />
         </div>
       </section>
-      <section className="w-1/3">
+      <section className="w-full xl:w-1/3">
         <div className="flex items-center gap-4">
-          <div className="flex justify-center items-center w-20 h-20 rounded-full bg-[var(--color-primary)] shadow-lg shadow-zinc-500/40">
-            <IconExpense size="w-14 h-14" color="#fff" />
+          <div className="flex justify-center items-center w-16 h-16 md:w-20 md:h-20 rounded-full bg-[var(--color-primary)] shadow-lg shadow-zinc-500/40">
+            <IconExpense size="w-10 h-10 md:w-14 md:h-14" color="#fff" />
           </div>
-          <h3 className="text-[1.4rem] font-titr text-black text-shadow-[2px_2px_4px_white] text-shadow-xl">
+          <h3 className="text-xl md:text-[1.4rem] font-titr text-black text-shadow-[2px_2px_4px_white] text-shadow-xl">
             دسته بندی برداشت ها
           </h3>
         </div>
