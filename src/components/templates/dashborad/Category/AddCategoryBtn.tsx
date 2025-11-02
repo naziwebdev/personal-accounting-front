03a6b9ca87567352b06703950bdfc70cf443bb57
nazi <nazipaeez777@gmail.com>
2025-11-02 21:58:36 +0330

    Do logic of add category

--- src/components/templates/dashborad/Category/AddCategoryBtn.tsx
+++ src/components/templates/dashborad/Category/AddCategoryBtn.tsx
@@ -11,14 +11,25 @@ import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { addCategory } from "@/validations/category";
 import { ICONS } from "../../../../config/categoryIcons";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useAuth } from "@/context/AuthContext";
+import { restoreAccessToken } from "@/utils/restoreAccessToken";
+import { toast } from "sonner";
 
 type FormValues = {
   icon: keyof typeof ICONS;
 };
 
+type addCategoryFormData = {
+  title: string;
+  type: "income" | "expense";
+  icon?: string | null | FormValues;
+};
+
 export default function AddCategoryBtn() {
   const [openModal, setOpenModal] = useState<boolean>(false);
-
+  const { accessToken, setAccessToken } = useAuth();
+  const queryClient = useQueryClient();
   const modalToggleHandle = () => setOpenModal(false);
 
   const {
@@ -39,6 +50,48 @@ export default function AddCategoryBtn() {
 
   const selectedIcon = watch("icon");
 
+  const addMutation = useMutation({
+    mutationFn: async (data: addCategoryFormData) => {
+      const makeRequest = async (token: string) => {
+        return await fetch("http://localhost:4002/api/v1/categories", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ ...data }),
+        });
+      };
+      let res = await makeRequest(accessToken!);
+      let result = await res.json();
+
+      if (result.statusCode === 401) {
+        const newToken = await restoreAccessToken();
+        if (!newToken) throw new Error("Unauthorized");
+        setAccessToken(newToken);
+        res = await makeRequest(newToken);
+        result = await res.json();
+      }
+
+      if (result.statusCode !== 201) throw new Error("Failed to add category");
+
+      return result.data;
+    },
+    onSuccess: () => {
+      toast.success("دسته بندی با موفقیت افزوده شد");
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+      reset();
+      modalToggleHandle();
+    },
+    onError: () => {
+      toast.error("خطا در افزودن دسته بندی");
+    },
+  });
+
+  const addCategoryHandle = (data: addCategoryFormData) => {
+    addMutation.mutate(data);
+  };
+
   return (
     <>
       <button
@@ -55,112 +108,115 @@ export default function AddCategoryBtn() {
               افزودن دسته بندی
             </h2>
             <form
-              // onSubmit={handleSubmit(addCardHandle)}
-              className="px-0 md:px-32 flex items-center justify-center flex-wrap gap-y-5 text-xs xs:text-base"
+              onSubmit={handleSubmit(addCategoryHandle)}
+              className="px-4 md:px-20 lg:px-0 flex items-center justify-center flex-wrap gap-y-5 text-xs xs:text-base"
             >
-              <div className="w-full">
-                <div className="flex items-center gap-2">
-                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
-                    <IconPaper size="w-6 h-6 xs:w-7 xs:h-7" color="#ffffff" />{" "}
+              <div className="w-full flex flex-wrap items-center gap-6 justify-between lg:justify-evenly">
+                <div className="w-full lg:max-w-5/12">
+                  <div className="flex items-center gap-2">
+                    <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
+                      <IconPaper size="w-6 h-6 xs:w-7 xs:h-7" color="#ffffff" />{" "}
+                    </div>
+                    <input
+                      {...register("title")}
+                      type="text"
+                      className="w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
+                      placeholder="نام دسته بندی را وارد کنید"
+                    />
                   </div>
-                  <input
-                    // {...register("bankName")}
-                    type="text"
-                    className="w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
-                    placeholder="نام دسته بندی را وارد کنید"
-                  />
-                </div>
 
-                <span className="text-right pt-1.5  text-sm  text-red-600">
-                  {/* {errors.bankName && errors.bankName.message} */}
-                </span>
-              </div>
-              <div className="w-full">
-                <div className="flex items-center gap-2">
-                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
-                    <IconCategory size="w-6 h-6 xs:w-7 xs:h-7" color="#fff" />
-                  </div>
-                  <Controller
-                    name="type"
-                    control={control}
-                    rules={{ required: true }}
-                    render={({ field }) => (
-                      <div className="relative w-full">
-                        <select
-                          {...field}
-                          value={field.value ?? "-1"}
-                          id="bankCardID"
-                          className="appearance-none w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
-                        >
-                          <option
-                            value={"-1"}
-                            className="bg-primary-p text-white"
+                  <span className="text-right pt-1.5  text-sm  text-red-600">
+                    {errors.title && errors.title.message}
+                  </span>
+                </div>
+                <div className="w-full lg:max-w-5/12">
+                  <div className="flex items-center gap-2">
+                    <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-secondary)]">
+                      <IconCategory size="w-6 h-6 xs:w-7 xs:h-7" color="#fff" />
+                    </div>
+                    <Controller
+                      name="type"
+                      control={control}
+                      rules={{ required: true }}
+                      render={({ field }) => (
+                        <div className="relative w-full">
+                          <select
+                            {...field}
+                            value={field.value ?? "-1"}
+                            id="bankCardID"
+                            className="appearance-none w-full bg-[var(--color-secondary)] p-3 placeholder:text-white rounded-xl text-white outline-0"
                           >
-                            نوع دسته بندی را وارد کنید
-                          </option>
-                          {/* {cards?.length !== 0 &&
-                            cards?.map((card) => (
-                              <option
-                                key={card?.id}
-                                value={card?.id}
-                                className="bg-[var(--color-primary)] text-white"
-                              >
-                                {card?.bankName} -{" "}
-                                {toPersianDigits(card?.cardNumber)
-                                  .match(/.{1,4}/g)
-                                  ?.join("-")}
-                              </option>
-                            ))} */}
-                        </select>
-                        <span className="absolute left-3 top-1/2 z-10 transform -translate-y-1/2 pointer-events-none">
-                          <IconDownArrow
-                            size="w-3 h-3 xs:w-4 xs:h-4"
-                            color="#fff"
-                          />
-                        </span>
-                      </div>
-                    )}
-                  />
-                </div>
-
-                <span className="text-right pt-1.5 text-sm  text-red-600">
-                  {/* {errors.cardNumber && errors.cardNumber.message} */}
-                </span>
-              </div>
-              <div className="w-full">
-                <div className="flex items-center gap-2">
-                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-theme)]">
-                    <IconEmoji size="w-6 h-6 xs:w-7 xs:h-7" color="#52525B" />
+                            <option value={"-1"} className=" text-white">
+                              نوع دسته بندی را وارد کنید
+                            </option>
+                            <option
+                              className="bg-[var(--color-primary)] text-white"
+                              value={"income"}
+                            >
+                              درامد
+                            </option>
+                            <option
+                              className="bg-[var(--color-primary)] text-white"
+                              value={"expense"}
+                            >
+                              هزینه
+                            </option>
+                          </select>
+                          <span className="absolute left-3 top-1/2 z-10 transform -translate-y-1/2 pointer-events-none">
+                            <IconDownArrow
+                              size="w-3 h-3 xs:w-4 xs:h-4"
+                              color="#fff"
+                            />
+                          </span>
+                        </div>
+                      )}
+                    />
                   </div>
-                  <p>یک ایکون انتخاب کنید (اختیاری) : </p>
-                  {Object.entries(ICONS).map(([key, { component: Icon }]) => (
-                    <label
-                      key={key}
-                      className={`cursor-pointer border rounded-xl p-4 flex flex-col items-center justify-center gap-2 transition ${
-                        selectedIcon === key
-                          ? "border-[var(--color-secondary)] bg-[var(--color-theme)]"
-                          : "border-gray-300"
-                      }`}
-                    >
-                      <input
-                        type="radio"
-                        value={key}
-                        {...register("icon")}
-                        className="hidden"
-                      />
-                      <Icon size="w-8 h-8" color="#333" />
-                      <span className="text-sm capitalize">{key}</span>
-                    </label>
-                  ))}
+
+                  <span className="text-right pt-1.5 text-sm  text-red-600">
+                    {errors.type && errors.type.message}
+                  </span>
+                </div>
+              </div>
+
+              <div className="w-full">
+                <div className="flex items-center flex-wrap justify-center gap-2 pt-4">
+                  <div className="flex items-center gap-x-2.5">
+                    <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-theme)]">
+                      <IconEmoji size="w-6 h-6 xs:w-7 xs:h-7" color="#52525B" />
+                    </div>
+                    <p>یک ایکون انتخاب کنید (اختیاری) : </p>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    {Object.entries(ICONS).map(([key, { component: Icon }]) => (
+                      <label
+                        key={key}
+                        className={`cursor-pointer border rounded-xl p-2 sm:p-4 flex items-center justify-center gap-2 transition ${
+                          selectedIcon === key
+                            ? "border-[var(--color-secondary)] bg-[var(--color-theme)]"
+                            : "border-gray-300"
+                        }`}
+                      >
+                        <input
+                          type="radio"
+                          value={key}
+                          {...register("icon")}
+                          className="hidden"
+                        />
+                        <Icon size="w-8 h-8" color="#333" />
+                      </label>
+                    ))}
+                  </div>
                 </div>
 
                 <span className="text-right pt-1.5  text-sm  text-red-600">
                   {/* {errors.balance && errors.balance.message} */}
                 </span>
               </div>
+
               <button
                 type="submit"
-                className="mt-7 w-1/2 md:w-1/4 h-10 xs:h-12 flex justify-center items-center text-white rounded-xl bg-[var(--color-primary)] text-base  xs:text-lg cursor-pointer"
+                className="mt-7 w-1/3 sm:w-1/5 h-10 xs:h-12 flex justify-center items-center text-white rounded-xl bg-[var(--color-primary)] text-base  xs:text-lg cursor-pointer"
               >
                 تایید
               </button>
