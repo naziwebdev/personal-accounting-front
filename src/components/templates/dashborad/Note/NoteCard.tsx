8fa8f0e8ce5dd947c6c080f3ebdd2bf6e2f7198b
nazi <nazipaeez777@gmail.com>
2025-11-04 22:13:35 +0330

    Do logic of update note

--- src/components/templates/dashborad/Note/NoteCard.tsx
+++ src/components/templates/dashborad/Note/NoteCard.tsx
@@ -10,6 +10,11 @@ import { useAuth } from "@/context/AuthContext";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { restoreAccessToken } from "@/utils/restoreAccessToken";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { editNote } from "@/validations/note";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { IconPaper } from "@/components/icons/IconPaper";
+import { IconDescription } from "@/components/icons/IconDescription";
 
 type NotePropType = {
   color: string;
@@ -19,6 +24,11 @@ type NotePropType = {
   title: string;
   description: string;
   createdAt: string;
+};
+
+type EditNoteFormData = {
+  title?: string | null;
+  description?: string | null;
 };
 
 export default function NoteCard({
@@ -32,12 +42,81 @@ export default function NoteCard({
 }: NotePropType) {
   const [toggleEditBtn, setToggleEditBtn] = useState<boolean>(false);
   const [isOpenNote, setIsOpenNote] = useState<boolean>(false);
+  const [openEditModal, setOpenEditModal] = useState<boolean>(false);
+
+  const editModalHandle = () => setOpenEditModal(false);
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title,
+      description,
+    },
+    resolver: yupResolver(editNote),
+  });
 
   const queryClient = useQueryClient();
   const { accessToken, setAccessToken } = useAuth();
   const router = useRouter();
 
   const closeNoteHanlde = () => setIsOpenNote(false);
+
+  /////// edit logic /////////
+  const editMutation = useMutation({
+    mutationFn: async (data: EditNoteFormData) => {
+      const makeRequest = async (token: string) => {
+        return await fetch(`http://localhost:4002/api/v1/notes/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          credentials: "include",
+          body: JSON.stringify({ ...data }),
+        });
+      };
+      let res = await makeRequest(accessToken!);
+
+      let result = await res.json();
+
+      if (result.statusCode === 401) {
+        const newToken = await restoreAccessToken();
+        if (!newToken) throw new Error("Unauthorized");
+        setAccessToken(newToken);
+        res = await makeRequest(newToken);
+        result = await res.json();
+      }
+      if (result.statusCode !== 200) throw new Error("Failed to edit note");
+
+      return result.data;
+    },
+
+    onSuccess: () => {
+      toast.success("یادداشت با موفقیت ویرایش شد");
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+      editModalHandle();
+    },
+    onError: () => {
+      toast.error("خطا در ویرایش  یادداشت");
+      editModalHandle();
+    },
+  });
+
+  const editNoteHandle = async (data: EditNoteFormData) => {
+    swal({
+      title: "آیا از ویرایش اطمینان دارید ؟",
+      icon: "warning",
+      buttons: ["خیر", "بله"],
+    }).then((value) => {
+      if (value) {
+        editMutation.mutate(data);
+      }
+    });
+  };
 
   //////// delete logic ///////
 
@@ -120,6 +199,7 @@ export default function NoteCard({
           {toggleEditBtn && (
             <>
               <button
+                onClick={() => setOpenEditModal(true)}
                 className="absolute flex justify-center items-center -right-7 top-8 cursor-pointer rounded-md bg-white px-2 py-0.5
             text-sm shadow-sm shadow-zinc-300/50"
               >
@@ -154,6 +234,63 @@ export default function NoteCard({
             <h3 className="text-xl font-black">متن یادداشت</h3>
             <p className="">{description}</p>
           </div>
+        </Modal>
+      )}
+      {openEditModal && (
+        <Modal onClose={editModalHandle}>
+          <>
+            <h2 className="w-1/2 md:w-1/5 mx-auto text-center pb-2 mb-12 text-lg xs:text-2xl font-bold  rounded-xl text-nowrap">
+              افزودن کارت
+            </h2>
+            <form
+              onSubmit={handleSubmit(editNoteHandle)}
+              className="px-0 md:px-32 flex items-center justify-center flex-wrap gap-y-5 text-xs xs:text-base"
+            >
+              <div className="w-full">
+                <div className="flex items-center gap-2">
+                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-theme)]">
+                    <IconPaper size="w-7 h-7 xs:w-8 xs:h-8" color="#52525B" />
+                  </div>
+                  <input
+                    {...register("title")}
+                    type="text"
+                    className="w-full bg-[var(--color-theme)] p-3 placeholder:text-[#52525B] rounded-xl text-[#52525B] outline-0"
+                    placeholder="عنوان یادداشت را وارد کنید"
+                  />
+                </div>
+
+                <span className="text-right pt-1.5  text-sm  text-red-600">
+                  {errors.title && errors.title.message}
+                </span>
+              </div>
+
+              <div className="w-full">
+                <div className="flex items-center gap-2">
+                  <div className="w-10 h-10 xs:w-12 xs:h-12 flex justify-center items-center rounded-full bg-[var(--color-theme)]">
+                    <IconDescription
+                      size="w-5 h-5 xs:w-6 xs:h-6"
+                      color="#52525B"
+                    />{" "}
+                  </div>
+                  <textarea
+                    {...register("description")}
+                    className="w-full bg-[var(--color-theme)] p-3 resize-none placeholder:text-[#52525B] rounded-xl text-[#52525B] outline-0"
+                    placeholder="متن یادداشت را وارد کنید"
+                  ></textarea>
+                </div>
+
+                <span className="text-right pt-1.5  text-sm  text-red-600">
+                  {errors.description && errors.description.message}
+                </span>
+              </div>
+              <button
+                type="submit"
+                className="mt-7 w-1/2 md:w-1/4 h-10 xs:h-12 flex justify-center items-center text-white rounded-xl bg-[var(--color-primary)] text-base  xs:text-lg cursor-pointer"
+              >
+                تایید
+              </button>
+            </form>
+          </>
         </Modal>
       )}
     </div>
